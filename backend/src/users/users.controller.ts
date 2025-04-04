03eaedc2b16413f583e89c047fa1c20071ae636f
Anthony Steffen <89876617+anthony-steffen@users.noreply.github.com>
2025-04-04 11:18:35 -0300

    ✨ feat: Implementing role access control.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
-@UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard) // Aplica autenticação JWT a todas as rotas do controlador
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
@@ -12,6 +14,8 @@ export class UsersController {
     return this.usersService.findAll();
   }
 
+  @UseGuards(RolesGuard) // Aqui basta aplicar apenas o RolesGuard
+  @Roles('admin')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(id);
