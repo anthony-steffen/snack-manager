d76820781667c34ac913c592da2bbc453a12870a
Anthony Steffen <89876617+anthony-steffen@users.noreply.github.com>
2025-04-06 22:46:12 -0300

    🐛fix: Fixing bug with update route

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -4,7 +4,9 @@ import {
   Get,
   Param,
   Patch,
+  Body,
   UseGuards,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -12,32 +14,35 @@ import { Roles } from 'src/auth/decorators/roles.decorator';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { UpdateUserRoleDto } from './dto/update-user-role.dto';
 
-@UseGuards(JwtAuthGuard) // Aplica autenticação JWT a todas as rotas do controlador
 @Controller('users')
+@UseGuards(JwtAuthGuard, RolesGuard) // aplica ambos os guards globalmente no controller
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
+  @Roles('ADMIN')
   findAll() {
     return this.usersService.findAll();
   }
 
-  @UseGuards(RolesGuard) // Aqui basta aplicar apenas o RolesGuard
-  @Roles('admin')
   @Get(':id')
-  findOne(@Param('id') id: number) {
+  @Roles('ADMIN')
+  findOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.findOne(id);
   }
-  @UseGuards(RolesGuard)
-  @Roles('admin')
-  @Patch(':id/role')
-  update(@Param('id') id: number, @Param() dto: UpdateUserRoleDto) {
+
+  @Patch(':id')
+  @Roles('ADMIN')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: UpdateUserRoleDto,
+  ) {
     return this.usersService.update(id, dto);
   }
-  @UseGuards(RolesGuard)
+
+  @Delete(':id')
   @Roles('ADMIN')
-  @Delete(':id/remove')
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseIntPipe) id: string) {
     return this.usersService.delete(id);
   }
 }
